1c87cd847c8b59674489d8eb935e0fac60b8ff95
Marco Antonio Azevedo de Araujo Arcoverde <125325138+marcoarc01@users.noreply.github.com>
2024-05-25 15:42:33 -0300

    Update Main.java

--- jogo_labirinto/src/main/Main.java
+++ jogo_labirinto/src/main/Main.java
@@ -4,21 +4,19 @@ import javax.swing.JFrame;
 
 public class Main {
 
-	public static void main(String[] args) {
+    public static void main(String[] args) {
+        JFrame window = new JFrame();
+        window.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE); // fechar a tela
+        window.setResizable(false); // mudar o tamanho da tela
+        window.setTitle("Jogo Labirinto"); // título
 
-		JFrame window = new JFrame();
-		window.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE); // fechar a tela
-		window.setResizable(false); // mudar o tamanho da tela
-		window.setTitle("Jogo Labirinto"); // titulo
-		
-		GamePanel gamePanel = new GamePanel();
-		window.add(gamePanel);
-		window.pack();
-		
-		window.setLocationRelativeTo(null); //display no meio da tela
-		window.setVisible(true); // tornar a tela visivel
-		
-		gamePanel.startGameThread();
-	}
+        GamePanel gamePanel = new GamePanel();
+        window.add(gamePanel);
+        window.pack();
 
+        window.setLocationRelativeTo(null); // display no meio da tela
+        window.setVisible(true); // tornar a tela visível
+
+        gamePanel.startGameThread();
+    }
 }
