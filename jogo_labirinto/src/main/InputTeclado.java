6febf722c4274f17a0954755367db6ebb50233b4
Marco Antonio Azevedo de Araujo Arcoverde <125325138+marcoarc01@users.noreply.github.com>
2024-05-25 15:42:55 -0300

    Update InputTeclado.java

--- jogo_labirinto/src/main/InputTeclado.java
+++ jogo_labirinto/src/main/InputTeclado.java
@@ -3,54 +3,53 @@ package main;
 import java.awt.event.KeyEvent;
 import java.awt.event.KeyListener;
 
-public class InputTeclado implements KeyListener{
+public class InputTeclado implements KeyListener {
 
-	public boolean cima, baixo, direita, esquerda; // variaveis para controlar a movimentacao do personagem
-	
-	@Override
-	public void keyTyped(KeyEvent e) {
-	}
+    public boolean cima, baixo, direita, esquerda; // variáveis para controlar a movimentação do personagem
 
-	@Override
-	public void keyPressed(KeyEvent e) {
-		int code = e.getKeyCode();
-		
-		if(code == KeyEvent.VK_A) {
-			esquerda = true;
-		}
-		
-		if(code == KeyEvent.VK_W) {
-			cima = true;
-		}
-		
-		if(code == KeyEvent.VK_S) {
-			baixo = true;
-		}
-		
-		if(code == KeyEvent.VK_D) {
-			direita = true;
-		}
-	}
+    @Override
+    public void keyTyped(KeyEvent e) {
+    }
 
-	@Override
-	public void keyReleased(KeyEvent e) {
-		int code = e.getKeyCode();
-		
-		if(code == KeyEvent.VK_A) {
-			esquerda = false;
-		}
-		
-		if(code == KeyEvent.VK_W) {
-			cima = false;
-		}
-		
-		if(code == KeyEvent.VK_S) {
-			baixo = false;
-		}
-		
-		if(code == KeyEvent.VK_D) {
-			direita = false;
-		}
-	}
+    @Override
+    public void keyPressed(KeyEvent e) {
+        int code = e.getKeyCode();
 
+        if (code == KeyEvent.VK_A) {
+            esquerda = true;
+        }
+
+        if (code == KeyEvent.VK_W) {
+            cima = true;
+        }
+
+        if (code == KeyEvent.VK_S) {
+            baixo = true;
+        }
+
+        if (code == KeyEvent.VK_D) {
+            direita = true;
+        }
+    }
+
+    @Override
+    public void keyReleased(KeyEvent e) {
+        int code = e.getKeyCode();
+
+        if (code == KeyEvent.VK_A) {
+            esquerda = false;
+        }
+
+        if (code == KeyEvent.VK_W) {
+            cima = false;
+        }
+
+        if (code == KeyEvent.VK_S) {
+            baixo = false;
+        }
+
+        if (code == KeyEvent.VK_D) {
+            direita = false;
+        }
+    }
 }
