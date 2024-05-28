a0526a4bc46f080dc55cf588af4a96fcc657ac54
marcoarc01 <marcowin122@gmail.com>
2024-05-28 07:54:50 -0300

    atualizacao da colisao do bau

--- jogo_labirinto/src/object/OBJ_Bau.java
+++ jogo_labirinto/src/object/OBJ_Bau.java
@@ -11,5 +11,12 @@ public class OBJ_Bau extends SuperObject {
         } catch (IOException e) {
             e.printStackTrace();
         }
+        solidArea.x = 16;
+        solidArea.y = 16;
+        solidArea.width = 16;
+        solidArea.height = 16;
+
+        solidAreaDefaultX = solidArea.x;
+        solidAreaDefaultY = solidArea.y;
     }
 }
